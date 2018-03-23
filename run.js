3fd9164479dd1fee3885778069c94c2e21ca8369
Redeagle48 <antoniopereira350@gmail.com>
2018-03-23 22:35:56 +0000

    New baseline is created with success

--- run.js
+++ run.js
@@ -3,37 +3,57 @@ const looksSame = require("looks-same");
 const fs = require("fs");
 const notifier = require("node-notifier");
 
-let screenshotPath = __dirname + "/screenshot.png",
-  baseline = __dirname + "/baseline.png";
+const currDate = (function() {
+    var currentdate = new Date();
+    return currentdate.getDate() + "-" +
+        (currentdate.getMonth() + 1) + "-" +
+        currentdate.getFullYear() + "@" +
+        currentdate.getHours() + "_" +
+        currentdate.getMinutes() + "_" +
+        currentdate.getSeconds();
+})();
 
-  webshot("google.com", screenshotPath, function(err) {
-  if (!fs.existsSync(baseline)) {
-    fs.rename(screenshotPath, baseline, function(err) {
-      if (err) throw err;
-      console.log("renamed complete");
+let screenshotPath = __dirname + "/screenshot_" + currDate + ".png",
+    baseline = __dirname + "/baseline.png";
+
+function renameToBaseline() {
+    fs.renameSync(screenshotPath, baseline, function(err) {
+        if (err) throw err;
+        console.log("renamed complete");
     });
-  } else {
-    // screenshot now saved to google.png
-    looksSame(screenshotPath, baseline, function(error, equal) {
-      if (!equal) {
-        // Object
-        notifier.notify({
-          title: "OutSystems New Stuff!!",
-          message: "Go check it out!"
-        });
+}
 
-        //Delete actual baseline
-        //Screenshot is the new baseline
-
-      }
-      else {
-        // Object
-        notifier.notify({
-          title: "No News from OutSystems!!",
-          message: "Maybe later"
+function createNewBaseline() {
+    if (fs.existsSync(baseline)) {
+        fs.unlinkSync(baseline, (err) => {
+            if (err) throw err;
+            console.log(baseline + ' was deleted');
         });
-      
-      } 
-    });
-  }
+    }
+    renameToBaseline();
+}
+
+webshot("google.com", screenshotPath, function(err) {
+    if (!fs.existsSync(baseline)) {
+        createNewBaseline();
+        notifier.notify({
+            title: "Baseline Created",
+            message: "A new baseline was created"
+        });
+    } else {
+        looksSame(screenshotPath, baseline, function(error, equal) {
+            if (!equal) {
+                createNewBaseline();
+                notifier.notify({
+                    title: "OutSystems New Stuff!!",
+                    message: "Go check it out!"
+                });
+            } else {
+                notifier.notify({
+                    title: "No News from OutSystems :(",
+                    message: "Maybe later"
+                });
+            }
+        });
+    }
 });
